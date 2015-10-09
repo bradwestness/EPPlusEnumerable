df2a50745bb7cd268dff3696e5b57b19c2233fb2
pwieszynski <mgpeter@gmail.com>
2015-10-09 14:59:31 +0200

    Add SpreadsheetExcludeFromOutputAttribute

---
+++ EPPlusEnumerable/SpreadsheetExcludeFromOutputAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace EPPlusEnumerable
+{
+    using System;
+
+    /// <summary>
+    /// Use this attribute to denote that the property is not used to create a column in an excel worksheet when exporting data.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property,AllowMultiple = false,Inherited = true)]
+    public class SpreadsheetExcludeFromOutputAttribute : Attribute
+    {   
+    }
+}
