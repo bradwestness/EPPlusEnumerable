0f7a617d935f1e2c8db8c764b004884f92db3ffe
aricept <aricept@users.noreply.github.com>
2019-11-15 10:47:49 -0600

    Update GetCustomAttribute extensions to support taking in type.
    In cases where attributes are stored on a metadata class, .GetCustomAttribute will not locate the attributes. We need to locate the MetadataTypeAttribute on the class, find the referenced type, and search that for attributes.

--- EPPlusEnumerable/Extensions.cs
+++ EPPlusEnumerable/Extensions.cs
@@ -5,14 +5,28 @@
 
     internal static class Extensions
     {
-        public static T GetCustomAttribute<T>(this MemberInfo element, bool inherit) where T : Attribute
+        public static T GetCustomAttribute<T>(this MemberInfo element, Type type, bool inherit) where T : Attribute
         {
-            return (T)Attribute.GetCustomAttribute(element, typeof(T), inherit);
+            var attr = (T)Attribute.GetCustomAttribute(element, typeof(T), inherit);
+            
+            if (attr == null)
+            {
+                return element.GetMetaAttribute<T>(type, inherit);
+            }
+            
+            return attr;
         }
 
-        public static T GetCustomAttribute<T>(this MemberInfo element) where T : Attribute
+        public static T GetCustomAttribute<T>(this MemberInfo element, Type type) where T : Attribute
         {
-            return (T)Attribute.GetCustomAttribute(element, typeof(T));
+            var attr = (T)Attribute.GetCustomAttribute(element, typeof(T));
+            
+            if (attr == null)
+            {
+                return element.GetMetaAttribute<T>(type);
+            }
+            
+            return attr;
         }
 
         public static object GetValue(this PropertyInfo element, Object obj)
